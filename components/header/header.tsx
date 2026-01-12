8838e0e8ef466d198667a0ad4533b66195eaaed6
HaJaehyeong <hajae305@gmail.com>
2026-01-12 15:23:59 +0900

    add tabs: in header

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,9 +1,28 @@
+'use client';
+import { NAV_TABS } from '@/constants/constants';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import styles from './header.module.scss';
 
 export default function Header() {
+  const pathname = usePathname();
+
   return (
     <header className={styles['header']}>
-      <nav className={styles['header__nav']}>JUST HAJAE</nav>
+      <nav className={styles['header__nav']}>
+        <div className={styles['header__nav__title']}>JUST HAJAE</div>
+        <div className={styles['header__nav__tabs']}>
+          {NAV_TABS.map((tab) => (
+            <Link
+              key={tab.id}
+              href={tab.url}
+              className={`${pathname === tab.url ? styles['header__nav__tabs--active'] : ''}`}
+            >
+              {tab.name}
+            </Link>
+          ))}
+        </div>
+      </nav>
     </header>
   );
 }
