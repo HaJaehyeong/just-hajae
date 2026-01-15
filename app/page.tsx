a3cbed8bc5d0add707f43cc33c45239373691db9
HaJaehyeong <hajae305@gmail.com>
2026-01-15 15:44:41 +0900

    modify styles: layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import ThreeColumnLayout from '@/components/three-column-layout/three-column-layout';
 
 export default function Home() {
-  return <ThreeColumnLayout left={<>abc</>} center={<>def</>} right={<>ghi</>} />;
+  return (
+    <ThreeColumnLayout
+      left={<>왼쪽입니다</>}
+      center={<>가운데입니다</>}
+      right={<>오른쪽입니다</>}
+    />
+  );
 }
