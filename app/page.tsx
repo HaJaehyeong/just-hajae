a6273425cd85b6871f77b7828c987f48bdf6565c
HaJaehyeong <hajae305@gmail.com>
2026-01-23 08:16:48 +0900

    [wip] add component: side nav

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,27 @@
 import LinkCard from '@/components/link-card/link-card';
+import SideNav from '@/components/side-nav/side-nav';
 import ThreeColumnLayout from '@/components/three-column-layout/three-column-layout';
 import { SHARE_LINK } from '@/constants/constants';
+import styles from './page.module.scss';
 
 export default function Home() {
   return (
     <ThreeColumnLayout
       left={
-        <>
-          {SHARE_LINK.map((link) => (
-            <LinkCard
-              key={link.key}
-              url={link.url}
-              title={link.title}
-              icon={link.icon}
-              isExternal
-            />
-          ))}
-        </>
+        <div className={styles['left']}>
+          <div>
+            {SHARE_LINK.map((link) => (
+              <LinkCard
+                key={link.key}
+                url={link.url}
+                title={link.title}
+                icon={link.icon}
+                isExternal
+              />
+            ))}
+          </div>
+          <SideNav />
+        </div>
       }
       center={<>hello</>}
       right={<>world</>}
