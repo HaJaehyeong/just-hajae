0d4c0e52c4e2302863cbaccb322b9556a2cfcdf7
HaJaehyeong <hajae305@gmail.com>
2026-01-23 09:13:24 +0900

    modify component: side nav frame

--- components/side-nav/side-nav.tsx
+++ components/side-nav/side-nav.tsx
@@ -1,3 +1,14 @@
+import styles from './side-nav.module.scss';
+
 export default function SideNav() {
-  return <>hello</>;
+  return (
+    <div className={styles['wrapper']}>
+      <div>TITLE</div>
+      <div>Nav Link</div>
+      <div>
+        <div>Tree Parents</div>
+        <div>Tree Childrends</div>
+      </div>
+    </div>
+  );
 }
